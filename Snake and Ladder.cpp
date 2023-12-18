d8e60f0e9a156a666897c19469a9049bb0ae29bf
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2023-12-19 00:13:23 +0530

    Update Snake and Ladder.cpp

--- Snake and Ladder.cpp
+++ Snake and Ladder.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
+using namespace std;
 void draw_line(int n, char ch);
 void board();
 void gamescore(const char name1[], const char name2[], int p1, int p2);
@@ -14,47 +14,47 @@ int main() {
     srand(static_cast<unsigned>(time(nullptr)));
 
     draw_line(50, '=');
-    std::cout << "\n\n\n\n\t\tSNAKE LADDER GAME\n\n\n\n";
+  cout << "\n\n\n\n\t\tSNAKE LADDER GAME\n\n\n\n";
     draw_line(50, '=');
-    std::cout << "\n\n\nEnter Name of player 1 :";
-    std::cin.getline(player1name, 80);
-    std::cout << "\n\n\Enter Name of player 2 :";
-    std::cin.getline(player2name, 80);
+    cout << "\n\n\nEnter Name of player 1 :";
+    cin.getline(player1name, 80);
+    cout << "\n\n\Enter Name of player 2 :";
+    cin.getline(player2name, 80);
 
     while (player1 < 100 && player2 < 100) {
         board();
         gamescore(player1name, player2name, player1, player2);
 
-        std::cout << "\n\n--->" << player1name << " Now your Turn >> Press Enter to play ";
-        std::cin.ignore();
+       cout << "\n\n--->" << player1name << " Now your Turn >> Press Enter to play ";
+    cin.ignore();
         lastposition = player1;
         play_dice(player1);
         if (player1 < lastposition)
-            std::cout << "\n\aOops!! Snake found !! You are at position " << player1 << "\n";
+            cout << "\n\aOops!! Snake found !! You are at position " << player1 << "\n";
         else if (player1 > lastposition + 6)
-            std::cout << "\nGreat!! you got a ladder !! You are at position " << player1;
+          cout << "\nGreat!! you got a ladder !! You are at position " << player1;
         
-        std::cout << "\n\n--->" << player2name << " Now your Turn >> Press Enter to play ";
-        std::cin.ignore();
+cout << "\n\n--->" << player2name << " Now your Turn >> Press Enter to play ";
+        cin.ignore();
         lastposition = player2;
         play_dice(player2);
         if (player2 < lastposition)
-            std::cout << "\n\n\aOops!! Snake found !! You are at position " << player2 << "\n";
+            cout << "\n\n\aOops!! Snake found !! You are at position " << player2 << "\n";
         else if (player2 > lastposition + 6)
-            std::cout << "\n\nGreat!! you got a ladder !! You are at position " << player2 << "\n";
+           cout << "\n\nGreat!! you got a ladder !! You are at position " << player2 << "\n";
     }
 
-    std::cout << "\n\n\n";
+    cout << "\n\n\n";
     draw_line(50, '+');
-    std::cout << "\n\n\t\tRESULT\n\n";
+    cout << "\n\n\t\tRESULT\n\n";
     draw_line(50, '+');
-    std::cout << std::endl;
+    cout << endl;
     gamescore(player1name, player2name, player1, player2);
-    std::cout << "\n\n\n";
+    cout << "\n\n\n";
     if (player1 >= player2)
-        std::cout << player1name << " !! You are the winner of the game\n\n";
+       cout << player1name << " !! You are the winner of the game\n\n";
     else
-        std::cout << player2name << " !! You are the winner of the game\n\n";
+        cout << player2name << " !! You are the winner of the game\n\n";
     draw_line(50, '+');
 
     return 0;
@@ -62,41 +62,41 @@ int main() {
 
 void draw_line(int n, char ch) {
     for (int i = 0; i < n; i++)
-        std::cout << ch;
+    cout << ch;
 }
 
 void board() {
-    std::system("clear");  // Use "cls" for Windows
-    std::cout << "\n\n";
+   system("clear");  // Use "cls" for Windows
+   cout << "\n\n";
     draw_line(50, '-');
-    std::cout << "\n\t\tSNAKE AT POSITION\n";
+  cout << "\n\t\tSNAKE AT POSITION\n";
     draw_line(50, '-');
-    std::cout << "\n\tFrom 98 to 28 \n\tFrom 95 to 24\n\tFrom 92 to 51\n\tFrom 83 to 19\n\tFrom 73 to 1\n\tFrom 69 to 33\n\tFrom 64 to 36\n\tFrom 59 to 17\n\tFrom 55 to 7\n\tFrom 52 to 11\n\tFrom 48 to 9\n\tFrom 46 to 5\n\tFrom 44 to 22\n\n";
+    cout << "\n\tFrom 98 to 28 \n\tFrom 95 to 24\n\tFrom 92 to 51\n\tFrom 83 to 19\n\tFrom 73 to 1\n\tFrom 69 to 33\n\tFrom 64 to 36\n\tFrom 59 to 17\n\tFrom 55 to 7\n\tFrom 52 to 11\n\tFrom 48 to 9\n\tFrom 46 to 5\n\tFrom 44 to 22\n\n";
     draw_line(50, '-');
-    std::cout << "\n\t\t LADDER AT POSITION\n";
+  cout << "\n\t\t LADDER AT POSITION\n";
     draw_line(50, '-');
-    std::cout << "\n\tFrom 8 to 26\n\tFrom 21 to 82\n\tFrom 43 to 77\n\tFrom 50 to 91\n\tFrom 62 to 96\n\tFrom 66 to 87\n\tFrom 80 to 100\n";
+    cout << "\n\tFrom 8 to 26\n\tFrom 21 to 82\n\tFrom 43 to 77\n\tFrom 50 to 91\n\tFrom 62 to 96\n\tFrom 66 to 87\n\tFrom 80 to 100\n";
     draw_line(50, '-');
-    std::cout << std::endl;
+    cout << endl;
 }
 
 void gamescore(const char name1[], const char name2[], int p1, int p2) {
-    std::cout << "\n";
+    cout << "\n";
     draw_line(50, '~');
-    std::cout << "\n\t\tGAME STATUS\n";
+   cout << "\n\t\tGAME STATUS\n";
     draw_line(50, '~');
-    std::cout << "\n\t--->" << name1 << " is at position " << p1 << std::endl;
-    std::cout << "\t--->" << name2 << " is at position " << p2 << std::endl;
+    cout << "\n\t--->" << name1 << " is at position " << p1 <<endl;
+   cout << "\t--->" << name2 << " is at position " << p2 << endl;
     draw_line(50, '_');
-    std::cout << std::endl;
+    cout << endl;
 }
 
 void play_dice(int &score) {
     int dice;
     dice = rand() % 6 + 1;
-    std::cout << "\nYou got " << dice << " Point !! ";
+   cout << "\nYou got " << dice << " Point !! ";
     score = score + dice;
-    std::cout << "Now you are at position " << score;
+   cout << "Now you are at position " << score;
     switch (score) {
         // Cases for snakes and ladders
         // ...
